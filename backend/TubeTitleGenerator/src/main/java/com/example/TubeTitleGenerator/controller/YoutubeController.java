c2b593bee02d2bb76b74b27e520a9a1aebb8e6e9
Abhi <abhi@Abhis-MacBook-Air.local>
2026-08-31 14:58:29 +0530

    updated backend code

--- backend/TubeTitleGenerator/src/main/java/com/example/TubeTitleGenerator/controller/YoutubeController.java
+++ backend/TubeTitleGenerator/src/main/java/com/example/TubeTitleGenerator/controller/YoutubeController.java
@@ -4,7 +4,11 @@ import com.example.TubeTitleGenerator.dto.GenerateRequest;
 import com.example.TubeTitleGenerator.dto.GenerateResponse;
 import com.example.TubeTitleGenerator.service.YoutubeGeneratorService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -19,4 +23,13 @@ public class YoutubeController {
 
         return service.generate(request);
     }
+
+    @GetMapping("/me")
+    public Object getCurrentUser(
+            @AuthenticationPrincipal OAuth2User user) {
+        return Map.of(
+                "name", user.getAttribute("name"),
+                "email", user.getAttribute("email"),
+                "picture", user.getAttribute("picture")
+        );    }
 }
